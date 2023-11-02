37bfc76444c32098f64753aef9252a99701e22e5
Almog Arazi <112971847+Almog-Arazi@users.noreply.github.com>
2023-11-02 15:39:43 +0200

    Updated

---
+++ script.js
@@ -0,0 +1,36 @@
+// Create a new SpeechSynthesisUtterance object
+let speech = new SpeechSynthesisUtterance();
+
+// Create an empty array to store available voices
+let voices = [];
+
+// Select the <select> element from the HTML document
+let voiceSelect = document.querySelector("select");
+
+// When the list of voices changes, this event handler is called
+window.speechSynthesis.onvoiceschanged = () => {
+  // Get the list of available voices from the Web Speech API
+  voices = window.speechSynthesis.getVoices();
+  
+  // Set the voice of the SpeechSynthesisUtterance object to the first voice in the list
+  speech.voice = voices[0];
+
+  // Populate the <select> element with voice options
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
+};
+
+// Add an event listener to the <select> element to change the voice when the user selects a different voice
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+// Add an event listener to the <button> element to start speaking the text
+document.querySelector("button").addEventListener("click", () => {
+  // Set the text to be spoken from the content of the <textarea> element
+  speech.text = document.querySelector("textarea").value;
+  
+  // Use the Web Speech API to speak the text
+  window.speechSynthesis.speak(speech);
+});
